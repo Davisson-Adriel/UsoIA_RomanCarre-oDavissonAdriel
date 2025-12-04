844cef8994ee7e2ba3884a2f734df7ee40800060
Davisson-Adriel <droman819@unab.edu.co>
2025-12-04 13:07:20 -0500

    feat: :sparkles: Filtrado con input
    Se agregan las lineas de codigo pertinentes para el funcionamiento del filtrado por input

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function obtener() {
                     const h2 = document.createElement("h2");
                     h2.textContent = personaje.name;
                     const estado = document.createElement("p");
-                    estado.textContent = personaje.status;
+                    estado.textContent = "Estado: " + personaje.status;
 
                     card.append(img, h2, estado);
                     contenedor.appendChild(card);
@@ -26,6 +26,41 @@ function obtener() {
         });
 };
 
-const linkAPI = "https://rickandmortyapi.com/api/character";
+function filtrar() {
+    const contenedor = document.getElementById("container");
+    contenedor.textContent = "Cargando...";
+    const filtro = document.getElementById("myInput").value;
+    let linkAPIFiltrado = linkAPI + `?name=${filtro}`;
 
+    fetch(linkAPIFiltrado)
+        .then(response => response.json())
+        .then(personajes => {
+
+            personajes = personajes.results;
+
+            setTimeout(() => {
+                contenedor.textContent = "";
+
+                personajes.forEach(personaje => {
+                    const card = document.createElement("div");
+                    card.className = "card";
+                    const img = document.createElement("img");
+                    img.src = personaje.image;
+                    const h2 = document.createElement("h2");
+                    h2.textContent = personaje.name;
+                    const estado = document.createElement("p");
+                    estado.textContent = "Estado: " + personaje.status;
+
+                    card.append(img, h2, estado);
+                    contenedor.appendChild(card);
+                });
+            }, 2000);
+        });
+}
+
+const linkAPI = "https://rickandmortyapi.com/api/character";
 obtener();
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementsByClassName("myButton")[0];
+    button.addEventListener("click", filtrar);
+});
